2b67013446cb639767f09a4b6dd37936d772677d
Mack Siu <msiu23@gmail.com>
2018-08-09 11:34:46 -0700

    revert

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,9 +86,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => 'support@zonily.com',
-   :password             => 'Zonily2018',
-   :authentication       => "login",
+   :user_name            => ENV['gmail_username'],
+   :password             => ENV['gmail_password'],
+   :authentication       => "plain",
    :enable_starttls_auto => true
   }
 end
